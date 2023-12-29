5b7c558ab1119469e4bde7b89765870cb7e147b0
Steven Lam <stevenl.inbox@gmail.com>
2023-12-28 22:22:36 -0800

    Missing Inv, Ans, Exponents, and Equals buttons functions

--- Calculator/index.js
+++ Calculator/index.js
@@ -2,30 +2,182 @@
 var RadianMode = true;
 var AnswerSection = document.getElementById('AnswerSection');
 var RadorDegEl = document.getElementById("RadorDeg");
+var AcEl = document.getElementById("AC");
+var allButtons = document.querySelectorAll('button');
+var πEl = document.getElementById("π");
+var eEl = document.getElementById("e");
+var EEl = document.getElementById("E");
+var FactorialEl = document.getElementById("!");
+var LeftParanthesesCount = 0;
 
-// Event listener setup for numeric buttons (0-9)
-for (var i = 0; i <= 9; i++) {
+// Event listeners setup
+
+allButtons.forEach(function (button) {
+    if (button.id == "RadorDeg"){
+        return
+    }
+    button.addEventListener('click', handleButtonClick);
+});
+
+for (let i = 0; i <= 9; i++) {
     var currentElement = document.getElementById(i.toString());
-    addClickListener(currentElement, i);
-}
-
-// Event listener setup for RadorDegEl
-if (RadorDegEl) {
-    RadorDegEl.addEventListener("click", function() {
-        toggleRadianMode();
-    });
-}
-
-// Function to add click event listener to an element
-function addClickListener(element, value) {
-    if (element) {
-        element.addEventListener("click", function() {
-            addNumber(value);
+    if (currentElement) {
+        currentElement.addEventListener("click", function() {
+            if (AnswerSection) {
+                AnswerSection.value += i;
+            }
         });
     }
 }
 
-// Function to handle toggling between Rad and Deg mode
+if (πEl) {
+    πEl.addEventListener("click", handlePiClick);
+}
+
+if (eEl) {
+    eEl.addEventListener("click", handleEClick);
+}
+
+if (EEl) {
+    EEl.addEventListener("click", handleEClick);
+}
+
+if (FactorialEl) {
+    FactorialEl.addEventListener("click", handleFactorialClick);
+}
+
+if (AcEl) {
+    AcEl.addEventListener("click", handleAcClick);
+}
+
+if (RadorDegEl) {
+    RadorDegEl.addEventListener("click", toggleRadianMode);
+}
+
+// Operators and parentheses event listeners
+var operators = ['+', '-', 'x', '÷', '.', '%'];
+operators.forEach(function (operator) {
+    addOperatorEventListener(operator);
+});
+
+var leftParanOperators = ['ln', 'log', 'sin', 'cos', 'tan', '√'];
+leftParanOperators.forEach(function (operator) {
+    addLeftParanOperator(operator);
+});
+
+var parentheses = ['(', ')'];
+parentheses.forEach(function (paren) {
+    addParantheses(paren);
+});
+
+// Functions
+function handleButtonClick(event) {
+    if (AnswerSection.value === "") {
+        AcEl.innerHTML = "CE";
+        return
+    }
+    var lastChar = AnswerSection.value.slice(-1);
+    if (lastChar == 'π' || lastChar == 'e') {
+        AnswerSection.value += "x";
+    }
+}
+
+function handlePiClick() {
+    if (AnswerSection) {
+        AnswerSection.value += 'π';
+    }
+}
+
+function handleEClick() {
+    if (AnswerSection) {
+        var lastChar = AnswerSection.value.charAt(AnswerSection.value.length - 1);
+        if (!lastChar.match(/[0-9]/)) {
+            return;
+        }
+        if (lastChar === 'E') {
+            return;
+        }
+        AnswerSection.value += 'E';
+    }
+}
+
+function handleFactorialClick() {
+    if (AnswerSection) {
+        var lastChar = AnswerSection.value.charAt(AnswerSection.value.length - 1);
+        if (!lastChar.match(/[0-9]/)) {
+            return;
+        }
+        if (lastChar === '!') {
+            return;
+        }
+        AnswerSection.value += '!';
+    }
+}
+
+function handleAcClick() {
+    if (AcEl.innerHTML === 'AC') {
+        AnswerSection.value = "";
+    } else {
+        AnswerSection.value = AnswerSection.value.slice(0, -1);
+        if (AnswerSection.value === "") {
+            AcEl.innerHTML = "AC";
+        }
+    }
+}
+
+function addOperatorEventListener(operator) {
+    if (document.getElementById(operator)) {
+        document.getElementById(operator).addEventListener("click", function () {
+            var currentExpression = AnswerSection.value;
+            if (currentExpression === "") {
+                if (operator === '-' || operator === '.') {
+                    AnswerSection.value = operator;
+                    return;
+                }
+            } else if (currentExpression === "-") {
+                if (operator === '.') {
+                    AnswerSection.value += operator;
+                }
+                return;
+            } else {
+                var lastChar = currentExpression.charAt(currentExpression.length - 1);
+                var isOperator = operators.includes(lastChar);
+                if (isOperator) {
+                    AnswerSection.value = currentExpression.slice(0, -1) + operator;
+                } else {
+                    AnswerSection.value += operator;
+                }
+            }
+        });
+    }
+}
+
+function addLeftParanOperator(operator) {
+    if (document.getElementById(operator)) {
+        document.getElementById(operator).addEventListener("click", function () {
+            AnswerSection.value += operator + "(";
+            LeftParanthesesCount++;
+        });
+    }
+}
+
+function addParantheses(paren) {
+    if (document.getElementById(paren)) {
+        document.getElementById(paren).addEventListener("click", function () {
+            if (paren === '(') {
+                AnswerSection.value += "(";
+                LeftParanthesesCount++;
+            } else {
+                if (LeftParanthesesCount <= 0) {
+                    return;
+                }
+                AnswerSection.value += ")";
+                LeftParanthesesCount--;
+            }
+        });
+    }
+}
+
 function toggleRadianMode() {
     if (RadorDegEl) {
         if (RadianMode) {
@@ -35,12 +187,5 @@ function toggleRadianMode() {
             RadorDegEl.innerHTML = "<b>Rad</b> | Deg";
             RadianMode = true;
         }
-    }
-}
-
-// Function to add a number to the AnswerSection
-function addNumber(val) {
-    if (AnswerSection) {
-        AnswerSection.value += val;
     }
 }
